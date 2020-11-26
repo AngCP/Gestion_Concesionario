e5271dab6f862c6aec1c0f953bc213eac51b0650
Ángela Coello Pulido <angela@MacBook-Air-de-Angela.local>
2020-11-26 20:13:08 +0100

    Añadida función searchCar();

--- Cliente.h
+++ Cliente.h
@@ -32,12 +32,20 @@ class Cliente{
 
         //Método searchCar():
             /*!
-                \brief Busca un coche en el parque del cliente.
+                \brief Busca un coche en el stock del cliente.
                 @returns el coche en cuestión, NULL si no lo encuentra.
 
             */
 
-    Coche searchCar(std::string licence);
+    Coche searchCar(std::string licence){
+        std:vector<Coche*>::iterator search_pos = _cars.begin();
+        while(search_pos != _cars.end()){
+            if((*search_pos)->getLicense() == license) //Si encuentra el coche.
+                return &*search_pos;
+                search_pos++;
+            return NULL;
+        }
+    }
 
         //Método addCar():
             /*!
